0c7a427ef7f1c2ff50a615e71dd599807e4ec108
RobertoCastanheira <roberto.castanheira@hydradev.io>
2023-03-08 18:24:52 +0000

    Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,21 @@
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+
+import { LoadingContext } from '../context/Loading';
+
 const Header = () => {
-  return <div>Header</div>;
+  const { isLoading } = useContext(LoadingContext);
+
+  return (
+    <header className="py-4 mb-4 border-b-2 flex justify-between items-center px-4">
+      <Link className="logo font-bold text-blue-600" to="/">
+        Podcaster
+      </Link>
+      {isLoading ? (
+        <div className="header-loading w-4 h-4 border-r-2 border-blue-900 rounded-full animate-spin"></div>
+      ) : null}
+    </header>
+  );
 };
 
 export default Header;

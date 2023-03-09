e187ce0b1cadb626e2a9c80429819fc3dd214e5b
RobertoCastanheira <roberto.castanheira@hydradev.io>
2023-03-09 10:05:00 +0000

    Fixed bug freezing on loading details

--- src/layouts/Details.jsx
+++ src/layouts/Details.jsx
@@ -1,23 +1,13 @@
-import { useContext } from 'react';
 import { Outlet } from 'react-router-dom';
-import { LoadingContext } from '../context/Loading';
 import Sidebar from '../components/Sidebar';
 
 const DetailsLayout = () => {
-  const { isLoading } = useContext(LoadingContext);
-
   return (
     <div className="flex gap-5 items-start">
-      {isLoading ? (
-        <div className="w-full h-[50vh] flex justify-center items-center">
-          <div className="header-loading w-10 h-10 border-r-2 border-blue-900 rounded-full animate-spin" />
-        </div>
-      ) : (
-        <>
-          <Sidebar />
-          <Outlet />
-        </>
-      )}
+      <>
+        <Sidebar />
+        <Outlet />
+      </>
     </div>
   );
 };
